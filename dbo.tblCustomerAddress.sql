7774698b2b21e32141956538640c3aac22066e0f
Declan Monaghan <P2432774@my365.dmu.ac.uk>
2020-03-02 14:50:30 +0000

    TABLE CHANGES

---
+++ dbo.tblCustomerAddress.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblCustomer] (
+    [Customer ID] INT        IDENTITY (1, 1) NOT NULL,
+    [Address] VARCHAR(50) NOT NULL, 
+    [FullName] VARCHAR(50) NOT NULL, 
+    [DOB] VARCHAR(30) NOT NULL, 
+    [Member] BIT NOT NULL, 
+    [Email] VARCHAR(30) NOT NULL, 
+    [Card Number] VARCHAR(19) NOT NULL, 
+    [Sort Code] VARCHAR(8) NOT NULL, 
+    CONSTRAINT [PK tblCustomer] PRIMARY KEY CLUSTERED ([Customer ID] ASC)
+);
+
