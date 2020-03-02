7c6de1571c9561c61e35003ca443bc7a108c3a55
Your Name <you@example.com>
2020-03-02 14:23:26 +0000

    Up to date.

--- AgileProductsLTDtesting/tstSoftware.cs
+++ AgileProductsLTDtesting/tstSoftware.cs
@@ -14,5 +14,41 @@ namespace AgileProductsLTDtesting
             Assert.IsNotNull(software);
        
         }
+
     }
+    [TestMethod]
+    public void NameSoftwareOK()
+    {
+        clsSoftware software = new clsSoftware();
+        string TestData = "faran";
+        software.softwareNameOK = TestData;
+        Assert.AreEqual(software.softwareNameOK, TestData);
+    }
+    [TestMethod]
+    public void DecriptionSoftwareOK()
+    {
+        clsSoftware software = new clsSoftware();
+        string TestData = "Microsoft";
+        software.descriptionSoftwareOK = TestData;
+        Assert.AreEqual(software.descriptionSoftwareOK, TestData);
+    }
+    [TestMethod]
+    public void PriceSoftwareOK()
+    {
+        clsSoftware software = new clsSoftware();
+        int TestData = 1;
+        software.Price = TestData;
+        Assert.AreEqual(software.Price, TestData);
+    }
+
+    [TestMethod]
+    public void ActivePropertyok()
+    {
+        clsSoftware software = new clsSoftware();
+        Boolean TestData = true;
+        software.Active = TestData;
+        Assert.AreEqual(software.Active, TestData);
+    }
+
+}
 }
--- Software_HardwareClasses/clsSoftware.cs
+++ Software_HardwareClasses/clsSoftware.cs
@@ -2,5 +2,16 @@
 {
     public class clsSoftware
     {
+        internal bool Instock;
+        internal bool Active;
+
+        public object softwareNameOK { get; internal set; }
+        public string SoftwareNameOK { get; internal set; }
+        public string Software { get; internal set; }
+        public string softwareDesciptionOK { get; internal set; }
+        public string softwareDescriptionOK { get; internal set; }
+        public string descriptionSoftwareOK { get; internal set; }
+        public int priceSoftwareOk { get; internal set; }
+        public int Price { get; internal set; }
     }
 }
