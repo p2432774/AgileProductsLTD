fe76f21b53ea5775bf1d34bafc31172dad559c4f
p2422743 <p2422743@my365.dmu.ac.uk>
2020-03-16 14:32:37 +0000

    fix

--- FrontEnd/AnStaff.aspx.cs
+++ FrontEnd/AnStaff.aspx.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Software_HardwareClasses;
 
 public partial class AnStaff : System.Web.UI.Page
 {
-    protected void btnOK_Click(object sender, EventArgs e)
+    protected void Page_Load(object sender, EventArgs e)
     {
+        clsStaff AnStaff = new clsStaff();
+        AnStaff = (clsStaff)Session["AnStaff"];
+        Response.Write(AnStaff.StaffID);
+    }
 
+    protected void btnOK_Click(object sender, EventArgs e)
+    { 
     }
 }
