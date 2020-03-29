188a1412e70d52dd34c16481a7f00f9e8e6a441d
P2435725 <P2435725@my365.dmu.ac.uk>
2020-03-29 21:55:50 +0100

    Changed File Name

---
+++ AgileProductsLTDTest/tstHardware.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Software_HardwareClasses;
+
+namespace AgileProductsLTDtesting
+{
+    [TestClass]
+    public class TstHardware
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsHardware hardware = new clsHardware();
+            Assert.IsNotNull(hardware);
+        }
+    }
+}
