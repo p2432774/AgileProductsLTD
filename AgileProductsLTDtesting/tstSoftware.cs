f9d92389773ef0b443426cd662239d92b1ad6e18
salimah <salimah.mohamed@dmu.ac.uk>
2020-03-31 18:23:46 +0100

    all find nethods have been re-written and fixed - you need tomake sure you are reading labs carefully and not missing things out! also added missing stored procedure.

--- AgileProductsLTDtesting/tstSoftware.cs
+++ AgileProductsLTDtesting/tstSoftware.cs
@@ -22,8 +22,8 @@ namespace AgileProductsLTDtesting
 
             clsSoftware software = new clsSoftware();
             Int32 TestData = 1;
-            software.SoftwareIDOK = TestData;
-            Assert.AreEqual(software.SoftwareIDOK, TestData);
+            software.SoftwareID = TestData;
+            Assert.AreEqual(software.SoftwareID, TestData);
         }
 
 
@@ -32,24 +32,24 @@ namespace AgileProductsLTDtesting
         {
             clsSoftware software = new clsSoftware();
             String TestData = "faran";
-            software.softwareNameOK = TestData;
-            Assert.AreEqual(software.softwareNameOK, TestData);
+            software.SoftwareName = TestData;
+            Assert.AreEqual(software.SoftwareName, TestData);
         }
         [TestMethod]
         public void SoftwareDescriptionOK()
         {
             clsSoftware software = new clsSoftware();
             String TestData = "Microsoft";
-            software.SoftwareDescriptionOK = TestData;
-            Assert.AreEqual(software.SoftwareDescriptionOK, TestData);
+            software.SoftwareDesciption = TestData;
+            Assert.AreEqual(software.SoftwareDesciption, TestData);
         }
         [TestMethod]
         public void SoftwarePriceOK()
         {
             clsSoftware software = new clsSoftware();
             Int32 TestData = 1;
-            software.SoftwarePriceOK = TestData;
-            Assert.AreEqual(software.SoftwarePriceOK, TestData);
+            software.SoftwarePrice = TestData;
+            Assert.AreEqual(software.SoftwarePrice, TestData);
         }
 
         [TestMethod]
@@ -69,6 +69,133 @@ namespace AgileProductsLTDtesting
             Boolean TestData = true;
             software.Instock = TestData;
             Assert.AreEqual(software.Instock, TestData);
+        }
+
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsSoftware software = new clsSoftware();
+            //boolean variable to store the result of the validation
+            Boolean Found = false;
+            //create some test data to use with the method
+            Int32 SoftwareID = 1;
+            //invoke the method
+            Found = software.Find(SoftwareID);
+            //test to see that the result is correct
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestSoftwareIDFound()
+        {
+            //create an instance of the class we want to create
+            clsSoftware software = new clsSoftware();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 SoftwareID = 1;
+            //invoke the method
+            Found = software.Find(SoftwareID);
+            //check the address no
+            if (software.SoftwareID != 1)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void TestSoftwareNameFound()
+        {
+            clsSoftware software = new clsSoftware();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 SoftwareID = 68;
+            Found = software.Find(SoftwareID);
+            if (software.SoftwareName != "Microsoft Office365")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestSoftwareDesciptionFound()
+        {
+            clsSoftware software = new clsSoftware();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 SoftwareID = 1;
+            Found = software.Find(SoftwareID);
+            if (software.SoftwareDesciption != "1232")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestSoftwarePriceFound()
+        {
+            //create an instance of the class we want to create
+            clsSoftware software = new clsSoftware();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 SoftwareID = 1;
+            //invoke the method
+            Found = software.Find(SoftwareID);
+            //check the address no
+            if (software.SoftwarePrice != 20.0000)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void TestDateofarrivalFound()
+        {
+            clsSoftware software = new clsSoftware();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 SoftwareID = 1;
+            Found = software.Find(SoftwareID);
+
+            if (software.Dateofarrival != Convert.ToDateTime("12/01/2019"))
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+
+      
+
+        [TestMethod]
+        public void TestInstockFound()
+        {
+            clsSoftware software = new clsSoftware();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 SoftwareID = 1;
+            Found = software.Find(SoftwareID);
+
+            if (software.Instock != true)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
         }
 
     }
--- Software_HardwareClasses/clsSoftware.cs
+++ Software_HardwareClasses/clsSoftware.cs
@@ -4,11 +4,82 @@ namespace Software_HardwareClasses
 {
     public class clsSoftware
     {
-        public int SoftwareIDOK { get; set; }
-        public bool Instock { get; set; }
-        public DateTime Dateofarrival { get; set; }
-        public int SoftwarePriceOK { get; set; }
-        public string SoftwareDescriptionOK { get; set; }
-        public string softwareNameOK { get; set; }
+
+        private Int32 mSoftwareID;
+        private DateTime mDateofarrival;
+        private Boolean mInstock;
+        private string mSoftwareName;
+        private string mSoftwareDesciption;
+        private Int32 mSoftwarePrice;
+
+        public int SoftwareID
+        {
+            get
+            {
+                return mSoftwareID;
+            }
+            set
+
+            {
+                mSoftwareID = value;
+            }
+        }
+        public bool Instock {
+            get
+            {
+                return mInstock;
+            }
+            set
+            {
+                mInstock = value;
+            }
+        }
+       
+        public int SoftwarePrice {
+            get
+            {
+                return mSoftwarePrice;
+            }
+            set {
+                mSoftwarePrice = value;
+            }
+        }
+
+        public string SoftwareName { get { return mSoftwareName; } set { mSoftwareName = value; } }
+
+        public DateTime Dateofarrival { get { return mDateofarrival; } set { mDateofarrival = value; } }
+
+        public string SoftwareDesciption { get { return mSoftwareDesciption; } set { mSoftwareDesciption = value; } }
+
+
+        public bool Find(int SoftwareID)
+        {
+
+            clsDataConnection DB = new clsDataConnection();
+
+            DB.AddParameter("@SoftwareID", SoftwareID);
+
+            DB.Execute("sproc_tblsoftware_FilterBySoftwareNo");
+
+            if (DB.Count == 1)
+            {
+
+                mSoftwareID = Convert.ToInt32(DB.DataTable.Rows[0]["SoftwareID"]);
+                mSoftwareName = Convert.ToString(DB.DataTable.Rows[0]["SoftwareName"]);
+                mSoftwareDesciption = Convert.ToString(DB.DataTable.Rows[0]["SoftwareDesciption"]);
+                mSoftwarePrice = Convert.ToInt32(DB.DataTable.Rows[0]["SoftwarePrice"]);
+                mDateofarrival = Convert.ToDateTime(DB.DataTable.Rows[0]["Dateofarrival"]);
+                mInstock = Convert.ToBoolean(DB.DataTable.Rows[0]["Instock"]);
+               
+
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+
+        }
     }
 }
