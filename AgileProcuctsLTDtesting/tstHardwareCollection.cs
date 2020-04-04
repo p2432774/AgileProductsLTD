9c89e5e02cb115ea19a0a7cac87d019af0087b03
Daniel O'Hara <P2435725@my365.dmu.ac.uk>
2020-04-04 21:53:39 +0100

    Week 23 started, tstHardwareCollection and clsHardwareCollection created.

---
+++ AgileProcuctsLTDtesting/tstHardwareCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Software_HardwareClasses;
+using System.Collections.Generic;
+
+namespace AgileProcuctsLTDtesting
+{
+    [TestClass]
+    public class tstHardwareCollection
+    {
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //Creates instance of clsHardwareCollection
+            clsHardwareCollection hardwarecollection = new clsHardwareCollection();
+            //Test to see if it exists
+            Assert.IsNotNull(AllAddresses);
+        }
+
+        [TestMethod]
+        public void HardwareListOK()
+        {
+            //Creates instance of clsHardwareCollection
+            clsHardwareCollection hardwareCollection = new clsHardwareCollection();
+        }
+    }
+}
